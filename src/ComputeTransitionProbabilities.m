349b756b9446fac783f71c0be1b37d03df66f56a
JaeyoungLim <gokoreas@gmail.com>
2016-12-04 21:39:01 +0100

    Implementation Compute Transition Probabilities
    Draft2

--- src/ComputeTransitionProbabilities.m
+++ src/ComputeTransitionProbabilities.m
@@ -42,19 +42,68 @@ function P = ComputeTransitionProbabilities( stateSpace, controlSpace, map, gate
 %           The entry P(i, j, l) represents the transition probability
 %           from state i to state j if control input l is applied.
 
-l = size(control_Space);
-[K, ~] = size(state_space);
-
+l = size(controlSpace,1);
+[K, ~] = size(stateSpace);
+[M, N] = size(map);
 P = zeros(K, K, l); %Initialize P
+u = [1, 0;
+    0, -1;
+    -1, 0;
+    0, 1;
+    0, 0];
+    
+    
 
-% Probability to be caught on Camera
+state_gate = find(ismember(stateSpace, gate), 1);
 
-P(~, j==gate, ~);
-
-% Probability to have a good picture
-P(:, j =, l);
-
-% Probability in moving to next state
-P(i, i+1, 1)
-
+for i=1:N
+   for j=1:M % State K+1 is the termination state
+       % find index of current state
+       if map(j, i)<=0
+        cur_state = find(ismember(stateSpace, [i, j]), 1);
+       else
+        continue;
+       end
+        
+       %Probability of getting good picture
+       % Check Probability to be caught on Each camera
+       for m=1: size(mansion, 1)
+            err_mansion = [j, i]-mansion(m);
+            if err_mansion(1)==0 || err_mansion(2)==0 %Camera is aligned with position
+                if err_mansion(2)==0
+                    %Values along the camera-position path
+                    path_mansion = map(j, min(i, i-sign(err_mansion)*norm(err_mansion(2))):max(i, i-sign(err_mansion(2))*norm(err), ));
+                elseif err_mansion(1)==0
+                    path_mansion = map(min(j, j-sign(err_mansion)*norm(err_mansion)):max(j, j-sign(err_mansion)*norm(err_mansion)), i);
+                else
+                    
+                end
+                if any(path_mansion(:)<=0) %There is no occlusion
+                    P_p = max(0.001, 0.5/norm(err_mansion));
+                end
+            end
+       end
+       
+       % Check Probability to be caught on Each camera
+       for c=1: size(cameras, 1)
+        err_cam = [j, i]-cameras(c);
+            if err_cam(2)==0 %Camera is aligned with position
+                %Values along the camera-position path
+                path_cam = map(j, min(i,i-sign(err_cam)*norm(err_cam(1))):max(i,i-sign(err_cam(1))*1));
+                if any(path_cam(:)<=0) %There is an occlusion
+                    P(cur_state, state_gate, :)=P(cur_state, state_gate, :)+cameras(c, 3)*norm(err_cam);
+                end
+            elseif err_cam(1)==0
+                path_cam = map(j:j-sign(err_cam(2))*norm(err_cam(2)), i);
+                if any(path_cam(:)<=0) %There is no occlusion
+                    P(cur_state, state_gate, :)=P(cur_state, state_gate, :)+cameras(c, 3)*norm(err_cam);
+                end
+            end                    
+       end
+       for l=4:1 %Probability for moving inputs        
+            if map(j+u(l,1), i+u(l,2) < 0) % Check if there are obstacle
+                P(cur_state, find(stateSpace, [j+u(l,1), i+u(l,2)]), l)=1-P(cur_state, state_gate, l);
+            end
+        end
+    end
 end
